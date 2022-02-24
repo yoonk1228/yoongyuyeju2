3e32ac214c2a6f9bccb5e80d57746a37c01dcc89
seungju <seungju121@naver.com>
2022-02-24 14:23:59 +0900

    db.js 왜하는지모르겟지만 commit

--- db.js
+++ db.js
@@ -1,13 +1,30 @@
 require('dotenv').config()
 const mysql = require('mysql2')
 
-const host = process.env.DB_HOST
+
+const host = process.env.DB_HOST || 'localhost'
 const user = process.env.DB_USER
 const password = process.env.DB_PASSWORD
-const database = process.env.DB_DATABASE
+const database = process.env.DB_DATABASE || 'YGYJ'
 
 const config = {host,user,password,database,connectionLimit: 10}
-
 const pool = mysql.createPool(config)
 
-module.exports = pool
+//여기부터 아래 주석까지 '규리' 만듦 user 에서 사용
+const pool2 = (cb) => {
+    pool.getConnection((err,conn)=>{
+        if(!err) {
+            cb(conn)
+        }
+
+        pool.releaseConnection(conn)
+    })
+}
+//여기까지
+module.exports = {
+    pool,
+    pool2
+}
+
+
+
