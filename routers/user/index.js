d895e211e2ac982a9f8288c79a15e0d1bcb2ccb3
bbb4756 <rlarbfl4756@gmail.com>
2022-02-24 16:28:46 +0900

    update라우터 연결

--- routers/user/index.js
+++ routers/user/index.js
@@ -3,28 +3,26 @@ const router = express.Router()
 const loginController = require('./login.controller')
 const profileController = require('./profile.controller')
 const signupController = require('./signup.controller')
-// const updateController = require('./update.controller')
+const updateController = require('./update.controller')
 const resignController = require('./resign.controller')
 
 router.get('/login',loginController.login)
 router.post('/login',loginController.loginAction)
 
+
 router.post('/logout', loginController.logoutAction)
+
 
 router.get('/signup', signupController.join)
 router.post('/signup', signupController.joinAction)
 
+
 router.get('/profile', profileController.profile)
 
 
-router.get('/update', (req,res)=>{
-    res.render('user/infoupdate')
-})
+router.get('/update', updateController.Update)
+router.post('/update', updateController.UpdateAction)
 
-
-router.post('/update', (req,res)=>{
-    res.send('회원정보수정 완료')
-})
 
 router.post('/resign', resignController.resignAction)
 
--- routers/user/update.controller.js
+++ routers/user/update.controller.js
@@ -1,3 +1,18 @@
 const db = require('../../db')
 const {alertmove} = require('../../util/alert')
 
+exports.Update = (req, res)=>{
+    res.render('user/infoupdate')
+}
+
+exports.UpdateAction = (req, res)=>{
+    console.log(req.session)
+    res.send('됐나요?')
+    // const index = req.query.index
+    // const view = list[index-1]
+    // res.render('board/update',{
+    //     list:view,
+    //     index:index
+    // })
+    
+}
