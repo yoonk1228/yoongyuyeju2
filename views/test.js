4b62b9796bf23d3d68920fbb1a0c669b1ee5a6d8
yoonk1228 <yoonk1228@naver.com>
2022-02-24 17:32:37 +0900

    front user page작성

---
+++ views/test.js
@@ -0,0 +1,11 @@
+const Swal = require('sweetalert2')
+// 알림창 이쁘게 만들기위한 임시 테스트파일
+// 기간내에 못할 수도 있음.. 나 화이팅..
+function winner() {
+  console.log('test')
+}
+// Swal.fire('Any fool can use a computer')
+
+module.exports = {
+  winner,
+}
