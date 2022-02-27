ff0d7c823f349a96435afc6d4fd7c8a4cf7a541b
bbb4756 <rlarbfl4756@gmail.com>
2022-02-27 18:41:20 +0900

    회원탈퇴 서버꺼짐문제 수정

--- routers/user/resign.controller.js
+++ routers/user/resign.controller.js
@@ -3,9 +3,10 @@ const {alertmove} = require('../../util/alert')
 
 exports.resignAction = (req, res) => {
     db.pool2(conn => {
-        // console.log(req.session)
+        
         console.log('req.body:' , req.body)
         conn.query(`delete from personal where userid = '${req.session.userid}'`,  (err, result)=>{
+            console.log("결과", result, err)
             if (result){
                 req.session.destroy(()=>{
                     req.session
@@ -13,8 +14,6 @@ exports.resignAction = (req, res) => {
                 res.send(alertmove('/', '회원탈퇴가 완료되었습니다. 메인페이지로 이동합니다.'))               
             }else if (err) throw err
             
-            console.log(err)
-            res.send(alertmove('/user/profile', '다시 시도해주세요.')) 
             
         })
     })
