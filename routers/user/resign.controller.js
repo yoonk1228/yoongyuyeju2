0a94eedef7785e68fc98fc0f5d89c60613189c1a
bbb4756 <rlarbfl4756@gmail.com>
2022-02-27 18:12:17 +0900

    회원탈퇴 수정

--- routers/user/resign.controller.js
+++ routers/user/resign.controller.js
@@ -10,10 +10,11 @@ exports.resignAction = (req, res) => {
                 req.session.destroy(()=>{
                     req.session
                 })
-                res.send(alertmove('회원탈퇴가 완료되었습니다'))               
+                res.send(alertmove('/', '회원탈퇴가 완료되었습니다. 메인페이지로 이동합니다.'))               
             }else if (err) throw err
             
             console.log(err)
+            
             
         })
     })
