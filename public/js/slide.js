66a3956d2aa28816aa95aaaabc90a13fe4544cea
yoonk1228 <yoonk1228@naver.com>
2022-02-27 15:41:18 +0900

    front slide.js 슬라이드 기능 구현

--- public/js/slide.js
+++ public/js/slide.js
@@ -1,41 +1,34 @@
-let imgPos = 0;
-let position = 0;
+let imgPos = 0
+let position = 0
 // img-area 크기
-const imgWidth = 500;
-const prevBtn = document.querySelector(".btn-prev")
-const nextBtn = document.querySelector('.btn-next')
+const imgWidth = 500
 const images = document.querySelector('.album')
 
-function prev() {
-    if (imgPos > 0) {
-        nextBtn.removeAttribute('disabled')
-        imgPos = imgPos - 1;
-        console.log('이미지 번호 = ' + imgPos)
-        position += imgWidth;
-        images.style.transform = `translateX(${position}px)`;
-    }
-    if (imgPos === 0 ) {
-        prevBtn.setAttribute('disabled', 'true')
+function plusposition() {
+    console.log('plus호출')
+    imgPos = imgPos + 1
+    console.log('이미지 번호 = ' + imgPos)
+    position -= imgWidth
+    images.style.transform = `translateX(${position}px)`
+}
+
+function minusposition() {
+    console.log('minus호출')
+    imgPos = imgPos - 4
+    position += imgWidth * 4
+    images.style.transform = `translateX(${position}px)`
+}
+
+function loop() {
+    setTimeout(loop, 2000)
+    if (imgPos >= 0 && imgPos < 4) {
+        plusposition()
+    } else if (imgPos === 4) {
+        minusposition()
     }
 }
 
-function next() {
-    if (imgPos < 3) {
-        prevBtn.removeAttribute('disabled')
-        imgPos = imgPos + 1;
-        console.log('이미지 번호 = ' + imgPos)
-        position -= imgWidth;
-        images.style.transform = `translateX(${position}px)`
-    }
-    if (imgPos === 3) {
-        nextBtn.setAttribute('disabled', 'true')
-    }
-}
-
-/* 데이터 초기화, 초기설정, 즉시적용. */
 function init() {
-    prevBtn.setAttribute('disabled', 'true')
-    prevBtn.addEventListener('click',prev)
-    nextBtn.addEventListener('click',next)
+    setTimeout(loop,2000)
 }
 init()
